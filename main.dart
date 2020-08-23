95a0c3fd42ca142e01ee205870b92a36743fde46
yasiru583 <61556791+yasiru583@users.noreply.github.com>
2020-08-23 07:56:21 +0530

    third

---
+++ main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:tweak/constants.dart';
+import 'package:tweak/intros/start.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of the application.
+
+  final int initPosition=0;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Tweak App-03',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: Start(initPosition),
+    );
+  }
+}
